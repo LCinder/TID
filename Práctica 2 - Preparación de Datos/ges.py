4970a728e52f6b7fe5532e3030147744da1d2c92
LCinder <diegohm1598@gmail.com>
2021-11-05 17:27:06 +0100

    Arreglado discretizacion

--- Práctica 2 - Preparación de Datos/ges.py
+++ Práctica 2 - Preparación de Datos/ges.py
@@ -5,13 +5,7 @@ from chefboost import Chefboost
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn import tree
 from sklearn.metrics import accuracy_score
-import pydotplus
-import matplotlib.pyplot as plot
-from PIL import Image
-import graphviz
-from caimcaim import CAIMD
 
 def loadDataset():
     data = pandas.read_excel("data/accidentes_reduced.xls")
@@ -28,7 +22,7 @@ def remove_columns(data, elements):
 
 if __name__ == "__main__":
     x, y = loadDataset()
-    x = remove_columns(x, ["WEEKDAY", "HOUR", "REL_JCT", "ALIGN", "PROFILE", "SUR_COND", "TRAF_CON",
+    x = remove_columns(x, ["WEEKDAY", "HOUR", "MAN_COL", "REL_JCT", "ALIGN", "PROFILE", "SUR_COND", "TRAF_CON",
     "SPD_LIM", "LGHT_CON", "WEATHER", "ALCOHOL"])
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6)
@@ -40,13 +34,14 @@ if __name__ == "__main__":
 
     print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
 
-    x_2_discr = x_train[["WKDY_I", "HOUR_I", "RELJCT_I"]]
-    discr = KBinsDiscretizer(n_bins=10, encode="ordinal", strategy="uniform")
-    x_disc = x_train
-    t = discr.fit_transform(x_2_discr)
-    x_disc[["WKDY_I", "HOUR_I", "RELJCT_I"]] = t #
+    elements = ["WKDY_I", "HOUR_I", "RELJCT_I", "MANCOL_I", "RELJCT_I", "ALIGN_I", "PROFIL_I", "SURCON_I", "TRFCON_I",
+    "SPDLIM_H", "LGTCON_I", "WEATHR_I", "ALCHL_I"]
 
-    ctree.fit(x_disc, y_train)
+    for element in elements:
+        discr = KBinsDiscretizer(n_bins=2, encode="ordinal", strategy="uniform")
+        x_train[element] = discr.fit_transform(x_train[element].values.reshape(-1, 1))
+
+    ctree.fit(x_train, y_train)
     y_pred = ctree.predict(x_test)
 
     print("Accuracy CAIM: " + str(accuracy_score(y_test, y_pred)))
