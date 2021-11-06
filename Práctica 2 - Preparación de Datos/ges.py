b9b57807e923c75b860b0bdbb4bb6ee76341862d
diego <diegohm1598@gmail.com>
2021-11-06 18:54:33 +0100

    Valores perdidos incluidos

--- Práctica 2 - Preparación de Datos/ges.py
+++ Práctica 2 - Preparación de Datos/ges.py
@@ -20,28 +20,80 @@ def remove_columns(data, elements):
         data = data.drop(element, axis="columns")
     return data
 
+def discretize(x_train):
+    x_train = remove_columns(x_train, imputed)
+    x_train_disc = x_train
+
+    # ["WKDY_I", "HOUR_I", "RELJCT_I", "MANCOL_I", "RELJCT_I", "ALIGN_I", "PROFIL_I", "SURCON_I", "TRFCON_I",
+    #"SPDLIM_H", "LGTCON_I", "WEATHR_I", "ALCHL_I"]
+    elements = ["HOUR_I", "SPDLIM_H", "WKDY_I"]
+    bins = [2, 5, 4]
+
+    accuracy_mean_discr = 0
+    accuracy_mean = 0
+
+    ctree = DecisionTreeClassifier(random_state=1)
+    n = 10
+
+    for variable, bin_i in zip(elements, bins):
+        discr = KBinsDiscretizer(n_bins=bin_i, encode="ordinal", strategy="uniform")
+
+        for i in range(n):
+            ctree.fit(x_train, y_train)
+            y_pred = ctree.predict(x_test)
+            accuracy_mean += accuracy_score(y_test, y_pred)
+
+            x_train_disc[variable] = discr.fit_transform(x_train[variable].values.reshape(-1, 1))
+            ctree.fit(x_train_disc, y_train)
+            y_pred = ctree.predict(x_test)
+            accuracy_mean_discr += accuracy_score(y_test, y_pred)
+
+    print("Accuracy: " + str(accuracy_mean / (n * len(elements))))
+    print("Accuracy CAIM: " + str(accuracy_mean_discr / (n * len(elements))))
+
+
+def loss_values():
+    x_train_imputed = remove_columns(x_train, not_imputed)
+    x_test_imputed = remove_columns(x_test, not_imputed)
+
+    ctree = DecisionTreeClassifier(random_state=1)
+    ctree.fit(x_train_imputed, y_train)
+    y_pred = ctree.predict(x_test_imputed)
+    print("Accuracy Imputed: " + str(accuracy_score(y_test, y_pred)))
+
+    x_train_not_imputed = remove_columns(x_train, imputed)
+    x_test_not_imputed = remove_columns(x_test, imputed)
+
+    ctree = DecisionTreeClassifier(random_state=1)
+    ctree.fit(x_train_not_imputed, y_train)
+    y_pred = ctree.predict(x_test_not_imputed)
+    print("Accuracy Not Imputed: " + str(accuracy_score(y_test, y_pred)))
+
+    #c) Imputar valores por media o moda
+    unknown = [9, 99, 9, 19, 99, 9, 9, 9, 29, 49, 99, 99, 9, 9, 9]
+
+    for variable, value in zip(not_imputed, unknown):
+        x_train_not_imputed[variable] =  x_train_not_imputed[variable].replace(value, None)
+
+        mean = x_train_not_imputed[variable].mean()
+        x_train_not_imputed[variable].fillna(value=mean)
+
+    tree = DecisionTreeClassifier(random_state=1)
+    ctree.fit(x_train_not_imputed, y_train)
+    y_pred = ctree.predict(x_test_not_imputed)
+    print("Accuracy Not Imputed Mean: " + str(accuracy_score(y_test, y_pred)))
+
 if __name__ == "__main__":
     x, y = loadDataset()
-    x = remove_columns(x, ["WEEKDAY", "HOUR", "MAN_COL", "REL_JCT", "ALIGN", "PROFILE", "SUR_COND", "TRAF_CON",
-    "SPD_LIM", "LGHT_CON", "WEATHER", "ALCOHOL"])
+    not_imputed = ["WEEKDAY", "HOUR", "MAN_COL", "REL_JCT", "ALIGN", "PROFILE", "SUR_COND", "TRAF_CON",
+    "SPD_LIM", "LGHT_CON", "WEATHER", "ALCOHOL"]
+    imputed =  ["WKDY_I", "HOUR_I", "MANCOL_I", "RELJCT_I", "ALIGN_I", "PROFIL_I", "SURCON_I", "TRFCON_I",
+    "SPDLIM_H", "LGTCON_I", "WEATHR_I", "ALCHL_I"]
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6)
 
-    ctree = DecisionTreeClassifier(random_state=1)
-    ctree.fit(x_train, y_train)
-
-    y_pred = ctree.predict(x_test)
-
-    print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
-
-    elements = ["WKDY_I", "HOUR_I", "RELJCT_I", "MANCOL_I", "RELJCT_I", "ALIGN_I", "PROFIL_I", "SURCON_I", "TRFCON_I",
-    "SPDLIM_H", "LGTCON_I", "WEATHR_I", "ALCHL_I"]
-
-    for element in elements:
-        discr = KBinsDiscretizer(n_bins=5, encode="ordinal", strategy="uniform")
-        x_train[element] = discr.fit_transform(x_train[element].values.reshape(-1, 1))
-
-    ctree.fit(x_train, y_train)
-    y_pred = ctree.predict(x_test)
-
-    print("Accuracy CAIM: " + str(accuracy_score(y_test, y_pred)))
+    #Ejercicio 1
+    #discretize(x_train)
+    #Ejercicio 2
+    #a
+    loss_values()
